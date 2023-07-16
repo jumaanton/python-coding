a65c3b5e99d1a97089e16fe011f634352fb758fc
Juma Antony <jumaantony840@gmail.com>
2023-07-16 16:48:50 +0300

    finished task 6

---
+++ python-hello_world/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for num1 in range(10):
+    for num2 in range(num1 + 1, 10):
+        if num1 < 9:
+            print("{:02d}, ".format(num1 * 10 + num2), end='')
+        else:
+            print("{:02d}".format(num1 * 10 + num2))
